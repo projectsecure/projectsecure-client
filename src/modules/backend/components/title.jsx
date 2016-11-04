5dca1a01751a2b06fb5574d48e69a4b2152d5400
Nico Knoll <mail@nico.is>
2016-11-04 13:19:54 +0100

    Inital commit

--- src/modules/backend/components/title.jsx
+++ src/modules/backend/components/title.jsx
@@ -6,27 +6,6 @@ class SectionTitle extends React.Component {
 		super(props);
 	}
 
-	getLocationUI() {
-		if(!this.props.location) return '';
-
-		let locationUI;
-		if(this.props.location == 'school') {
-			locationUI = (
-				<div className="location-info">
-					<i className="fa fa-graduation-cap" /> <span>in der Schule</span>
-				</div>
-			);
-		} else {
-			locationUI = (
-				<div className="location-info">
-					<i className="fa fa-home" /> <span>zu Hause</span>
-				</div>
-			);
-		}
-
-		return locationUI;
-	}
-
 	getSearchFieldUI() {
 		return (
 			<div className="search-wrapper">
@@ -47,7 +26,6 @@ class SectionTitle extends React.Component {
 					<div className="row">
 						<div className="col-sm-9 no-padding">
 							<h4>{this.props.title}</h4>
-							{this.getLocationUI.bind(this)()}
 						</div>
 						<div className="col-sm-3 no-padding">
 							{this.getSearchFieldUI.bind(this)()}
--- src/modules/login/containers/login.js
+++ src/modules/login/containers/login.js
@@ -1,40 +1,13 @@
-
 import {render} from 'react-dom';
 import {compose} from 'react-komposer';
 
 import component from '../components/login';
 import actions from '../actions/login';
 
-
-const schools =  {
-	'123': {
-		_id: '123',
-		name:'Schiller-Oberschule',
-		systems: [{
-			_id: '456',
-			name:'Moodle'
-		}]
-	},
-	'dsfdsf': {
-		_id: 'dsfdsf',
-		name:'Friedensburg Oberschule',
-		systems: [{
-			_id: 'asdasd',
-			name:'Moodle'
-		},
-		{
-			_id: 'sdfsdf',
-			name:'ITSLearning'
-		}]
-	}
-};
-
-
 const composer = (props, onData) => {
 
 	let componentData = {
-		actions,
-		schools
+		actions
 	};
 
 	onData(null, componentData);
