b72046afa08706b835c7f9889d3387b0a52de165
Nico Knoll <mail@nico.is>
2016-12-26 11:00:07 +0100

    use env for api url

--- src/modules/core/helpers/request.js
+++ src/modules/core/helpers/request.js
@@ -1,6 +1,6 @@
 import superagent from 'superagent';
 
-const API_URL = 'http://localhost:8000/api/';
+const API_URL = process.env.API_URL || 'http://localhost:8000/api/';
 
 export default {
 	del: function(path, fn) {
